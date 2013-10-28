8318801616f1eacf1860669664bfed0b8c26e4df
Logan Petersen <lpetersen@java.nas.local>
2013-10-28 10:19:06 -0400

    fixed main method

--- countdown.java
+++ countdown.java
@@ -1,16 +1,15 @@
 class countdown {
 	public static void main(String[] args){
 	System.out.println();
-	  int x = 99;
-	  int s = 0;
-	  countdown();
+	  int n = 99;
+	  countdown(99);
      }
-	public static void countdown(int x, int s){
-	  if (s<0){
+	public static void countdown(int n){
+	  if (n == 0){
 	    System.out.println(0);
-	 } else { 
-	  System.out.print(x-1);
-	  countdown(x-1);
+	 } else{
+	  System.out.println(n);
+	  countdown(n-1);
      }
    }
 }
